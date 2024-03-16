fd3d5b0d156d2ade8d3336369f07dfde192c844c
Matthew Nelson <developer@matthewnelson.io>
2024-03-16 08:23:23 -0400

    Update dependencies to latest stable versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,14 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 plugins {
     alias(libs.plugins.multiplatform) apply(false)
     alias(libs.plugins.binaryCompat)
-    alias(libs.plugins.gradleVersions)
 }
 
 allprojects {
@@ -39,44 +37,10 @@ plugins.withType<YarnPlugin> {
     the<YarnRootExtension>().lockFileDirectory = rootDir.resolve(".kotlin-js-store")
 }
 
-@Suppress("LocalVariableName")
 apiValidation {
-    val CHECK_PUBLICATION = findProperty("CHECK_PUBLICATION") as? String
-
-    if (CHECK_PUBLICATION != null) {
+    if (findProperty("CHECK_PUBLICATION") != null) {
         ignoredProjects.add("check-publication")
     } else {
         ignoredProjects.add("sample")
-    }
-}
-
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    // Example 1: reject all non stable versions
-    rejectVersionIf {
-        isNonStable(candidate.version)
-    }
-
-    // Example 2: disallow release candidates as upgradable versions from stable versions
-    rejectVersionIf {
-        isNonStable(candidate.version) && !isNonStable(currentVersion)
-    }
-
-    // Example 3: using the full syntax
-    resolutionStrategy {
-        componentSelection {
-            @Suppress("RedundantSamConstructor")
-            all(Action {
-                if (isNonStable(candidate.version) && !isNonStable(currentVersion)) {
-                    reject("Release candidate")
-                }
-            })
-        }
     }
 }
