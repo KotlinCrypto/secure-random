b609857e76e535b51255df5922c08975d66ca336
Matthew Nelson <developer@matthewnelson.io>
2024-08-30 13:46:34 -0400

    Enable build option useUniqueModuleNames

--- build-logic/src/main/kotlin/-KmpConfigurationExtension.kt
+++ build-logic/src/main/kotlin/-KmpConfigurationExtension.kt
@@ -30,6 +30,10 @@ fun KmpConfigurationExtension.configureShared(
     }
 
     configure {
+        options {
+            useUniqueModuleNames = true
+        }
+
         jvm {
             kotlinJvmTarget = JavaVersion.VERSION_1_8
             compileSourceCompatibility = JavaVersion.VERSION_1_8
